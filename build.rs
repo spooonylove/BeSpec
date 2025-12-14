b602ec72c62acdc54ced676ee4b1d5598fa2cc9e
BeSpec-Dev <35648267+BeSpec-Dev@users.noreply.github.com>
2025-12-13 20:24:27 -0500

    feat: Add windows icon, build script

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main () {
+    // Only run this on Windows builds
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.compile().unwrap();
+    }
+}
